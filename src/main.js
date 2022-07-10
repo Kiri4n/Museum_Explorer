e33cac8e67cd4fdb8fcb8734961622b76af7fe38
Kiri4n <kirian.jourand@gmail.com>
2022-07-10 09:39:24 +0200

    🎉 init commit

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preloads/preload.js')
+        }
+    })
+
+    mainWindow.loadFile('src/views/index.html')
+
+    // Open development tools.
+    mainWindow.webContents.openDevTools()
+}
+
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
